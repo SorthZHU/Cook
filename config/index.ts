b384a4097ba882bd990cf8f2cf1e05c6ec5a21a3
zouyujii <zouyujii@servyou.com.cn>
2025-12-30 09:52:37 +0800

    refactor(config): 移除未使用的参数并更新webpack5编译器配置
    移除defineConfig中未使用的command和mode参数
    将compiler配置从字符串更新为对象，并禁用prebundle功能
    移除mini.compileDependencies中不必要的依赖项

--- config/index.ts
+++ config/index.ts
@@ -4,7 +4,7 @@ import devConfig from './dev'
 import prodConfig from './prod'
 
 // https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
-export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
+export default defineConfig<'webpack5'>(async (merge) => {
   const baseConfig: UserConfigExport<'webpack5'> = {
     projectName: 'tryToCook',
     date: '2025-10-23',
@@ -29,7 +29,12 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       }
     },
     framework: 'react',
-    compiler: 'webpack5',
+    compiler: {
+      type: 'webpack5',
+      prebundle: {
+        enable: false
+      }
+    },
     babel: {
       plugins: [
         '@babel/plugin-transform-optional-chaining',
@@ -40,7 +45,6 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
     mini: {
-      compileDependencies: ['axios', 'mobx', 'mobx-react-lite'],
       postcss: {
         pxtransform: {
           enable: true,
